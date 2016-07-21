c873d7eea1112d739f27a4206f530cab1b1ee356
frol <frol.mr@ya.ru>
2016-07-21 16:11:37 +0300

    Add, edit, delete actions for cards controller, related views; Validations for model Card; simple_form gem are added to the app.

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'pg', '~> 0.18.4'
+gem 'simple_form', '~> 3.2', '>= 3.2.1'
 
 group :development, :test do
   gem 'byebug'
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,24 @@
 class Card < ActiveRecord::Base
+  before_validation :add_review_date, on: :create
+
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  after_validation :compare_words
+
+  private
+
+    def add_review_date
+      self.review_date = Time.now + 3.days
+    end
+
+    def word_processing(word)
+      word.gsub(/\W/, '').downcase
+    end
+
+    def compare_words
+      if word_processing(self.original_text) == word_processing(self.translated_text)
+        errors.add(:original_text, "оригинальный текст совпадает с переводом")
+        errors.add(:translated_text, "перевод совпадает с оригинальным текстом")
+      end
+    end
 end
