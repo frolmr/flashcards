82d6dcd565fab52b0830dea7f07693759e46b173
frol <frol.mr@ya.ru>
2016-07-24 13:58:18 +0300

    KISS for seed.rb script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,21 @@
 require 'nokogiri'
 require 'open-uri'
 
-DOMAIN = 'http://www.languagedaily.com'.freeze
-START_PAGE = DOMAIN + '/learn-german/vocabulary/common-german-words'
-SEARCH_PATTERN = %r{\A/learn-german/vocabulary/[a-z-]*\d*\z}
-ORIGINAL_WORD_TAG = 'td.bigLetter'.freeze
-TRANSLATED_TEXT_TAG = 'td.bigLetter + td'.freeze
-EMPTY_STRING_PATTERN = /\u00A0/
+start_page = Nokogiri::HTML(open('http://www.languagedaily.com/learn-german/vocabulary/common-german-words'))
 
-def open_page(link)
-  Nokogiri::HTML(open(link))
+get_all_links = start_page.css('a').map { |link| link['href'] }.uniq
+
+get_required_links = get_all_links.select { |link| link =~ %r{\A/learn-german/vocabulary/[a-z-]*\d*\z} }.map { |link| 'http://www.languagedaily.com' + link }
+
+result = {}
+get_required_links.each do |url|
+  page = Nokogiri::HTML(open(url))
+  original_words = page.css('td.bigLetter').map(&:text)
+  translated_text = page.css('td.bigLetter + td').map(&:text)
+  result.merge!(original_words.zip(translated_text).to_h)
 end
 
-def get_links_hrefs(page)
-  page.css('a').map { |link| link['href'] }.uniq
-end
-
-def find_required_links(links, link_pattern)
-  links.select { |link| link =~ link_pattern }.map { |link| DOMAIN + link }
-end
-
-def get_element_text(element)
-  element.map(&:text)
-end
-
-def get_all_words_from_all_pages(route)
-  result = {}
-  route.each do |url|
-    current_page = open_page(url)
-    result.merge!(find_words(current_page))
-  end
-  result
-end
-
-def find_words(page)
-  original_words = get_element_text(page.css(ORIGINAL_WORD_TAG))
-  translated_text = get_element_text(page.css(TRANSLATED_TEXT_TAG))
-  original_words.zip(translated_text).to_h
-end
-
-def remove_empty_words(words, empty_string_pattern)
-  words.delete_if { |_key, value| value =~ empty_string_pattern }
-end
-
-page = open_page(START_PAGE)
-links = get_links_hrefs(page)
-route = find_required_links(links, SEARCH_PATTERN)
-words = get_all_words_from_all_pages(route)
-result = remove_empty_words(words, EMPTY_STRING_PATTERN)
+result.delete_if { |_key, value| value =~ /\u00A0/ }
 
 result.each do |original, translated|
   Card.create(original_text: original, translated_text: translated)
