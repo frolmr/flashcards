95ab529b219931b63ee6b4c810e4dce01dd927b0
frol <frol.mr@ya.ru>
2016-08-01 00:47:02 +0300

    Simplify seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,9 @@ get_all_links = start_page.css('a').map { |link| link['href'] }.uniq
 
 get_required_links = get_all_links.select { |link| link =~ %r{\A/learn-german/vocabulary/[a-z-]*\d*\z} }.map { |link| 'http://www.languagedaily.com' + link }
 
-result = {}
 get_required_links.each do |url|
   page = Nokogiri::HTML(open(url))
-  original_words = page.css('td.bigLetter').map(&:text)
-  translated_text = page.css('td.bigLetter + td').map(&:text)
-  result.merge!(original_words.zip(translated_text).to_h)
-end
-
-result.delete_if { |_key, value| value =~ /\u00A0/ }
-
-result.each do |original, translated|
-  Card.create(original_text: original, translated_text: translated)
-  puts "*New record:\n #{original} -> #{translated}\nwas created in DB"
+  page.css('tr').drop(1).each do |word|
+    Card.create(original_text: word.xpath('td[2]').text, translated_text: word.xpath('td[3]').text) if word.xpath('td[3]').text !~ /\u00A0/
+  end
 end
