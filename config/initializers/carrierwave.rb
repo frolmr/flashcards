500f93d92f9fa3e92c09512396da2d830487ef5c
frol <frol.mr@ya.ru>
2016-09-23 17:12:31 +0300

    change storage for test env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,3 +7,10 @@ CarrierWave.configure do |config|
   }
   config.aws_bucket = 'frolflashcards'
 end
+
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
