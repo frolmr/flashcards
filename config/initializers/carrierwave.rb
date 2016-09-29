85e9e0fc3695369f7c1b4e0c149dd9c80422a7ce
frol <frol.mr@ya.ru>
2016-09-29 16:01:27 +0300

    test values for s3, for travis to pass

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,14 @@ CarrierWave.configure do |config|
   # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV['REGION']
+    # aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    # aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    # region:                ENV['REGION']
+    aws_access_key_id: 'test',
+    aws_secret_access_key: 'test-test',
+    region: 'mordor'
   }
-  config.fog_directory  = ENV['FOLDER']
+  # config.fog_directory  = ENV['FOLDER']
+  config.fog_directory = 'trash'
   config.fog_public     = false
 end
