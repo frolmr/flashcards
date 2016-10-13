45986df73305e2322989e44b4d07baa38702c87a
frol <frol.mr@ya.ru>
2016-10-14 00:12:12 +0300

    New unit-test for card model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -17,4 +17,36 @@ RSpec.describe Card, type: :model do
     expect(subject.card_check('HelLO mY. FrIenD?!')).to be true
     expect(subject.card_check('hello mu Friend')).to be false
   end
+
+  context 'has correct work of increase_review_date method' do
+    it 'should increase the review date' do
+      expect(subject.increase_review_date.to_i).to be > Time.now.to_i
+      expect(subject.increase_review_date.to_i).to_not be <= Time.now.to_i
+    end
+
+    it 'should move card to another box' do
+      expect{subject.increase_review_date}.to change{subject.box}.by(1)
+    end
+  end
+
+  context 'has correct work of decrease_review_date method' do
+    it 'should refresh the box number to one' do
+      expect{subject.decrease_review_date}.to change{subject.box}.to(1)
+    end
+
+    it 'should set the review date to 12 hours after current time' do
+      expect(subject.decrease_review_date.to_i / 100).to eq((Time.now + 12.hours).to_i / 100)
+    end
+  end
+
+  context 'has correct work of check_fail method' do
+    it 'should increment amount of bad_tries' do
+      expect{subject.check_fail}.to change{subject.bad_tries}.from(0).to(1)
+    end
+
+    it 'should refresh the bad_tries to 0 after three bad tries' do
+      subject.bad_tries = 2
+      expect{subject.check_fail}.to change{subject.bad_tries}.to(0)
+    end
+  end
 end
